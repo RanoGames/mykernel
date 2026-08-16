e0f25f0a46cdf402059f6a3c4cb7ae294c6e9219
Max <65811349+RanoGames@users.noreply.github.com>
2026-08-16 16:59:19 +0500

    Restore arrow key codes for line editing and history

--- src/keyboard.h
+++ src/keyboard.h
@@ -5,14 +5,19 @@
 
 void keyboard_init(void);
 
-/* Блокирующее чтение одного символа: "спит" (крутится в hlt),
- * пока пользователь не нажмёт клавишу, соответствующую печатаемому
- * символу. Возвращает ASCII-код символа, либо специальные коды
- * ниже для служебных клавиш. */
+/* Блокирующее чтение одного символа: ждёт, пока пользователь не
+ * нажмёт клавишу (PS/2 или serial при -nographic). Возвращает ASCII
+ * или специальные коды ниже. */
 char keyboard_getchar(void);
 
 /* Специальные "символы", которые может вернуть keyboard_getchar() */
 #define KEY_BACKSPACE '\b'
 #define KEY_ENTER     '\n'
+
+/* Коды стрелок (значения вне обычного ASCII) */
+#define KEY_UP        ((char)0x11)
+#define KEY_DOWN      ((char)0x12)
+#define KEY_LEFT      ((char)0x13)
+#define KEY_RIGHT     ((char)0x14)
 
 #endif
