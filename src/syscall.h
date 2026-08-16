39cb3fe38ceb4fa414b8e7f98b1d1beae0492afc
Max <65811349+RanoGames@users.noreply.github.com>
2026-08-16 18:40:14 +0500

    Add SYS_BRK and user heap helpers

--- src/syscall.h
+++ src/syscall.h
@@ -6,14 +6,19 @@
 #include "isr.h"
 #include <stdint.h>
 
+/* Номера как в Linux i386 */
 #define SYS_EXIT  1
-#define SYS_WRITE 4
 #define SYS_READ  3
+#define SYS_WRITE 4
+#define SYS_BRK   45
 
 void syscall_handler(struct registers* regs);
 
 void process_save_kernel_context(uint32_t esp, void* cont);
 void process_exit_to_kernel(int code);
 int process_last_exit_code(void);
+
+/* сброс userspace brk перед exec */
+void user_heap_reset(void);
 
 #endif
