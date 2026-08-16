b22c9bfdba580d758a5edd8d53ef961f3ac786f3
Max <65811349+RanoGames@users.noreply.github.com>
2026-08-16 19:38:49 +0500

    Add Linux-style errno constants

---
+++ src/errno.h
@@ -0,0 +1,35 @@
+/* errno.h — коды ошибок в стиле Linux (для syscall return = -errno) */
+#ifndef KERNEL_ERRNO_H
+#define KERNEL_ERRNO_H
+
+#define EPERM        1
+#define ENOENT       2
+#define ESRCH        3
+#define EINTR        4
+#define EIO          5
+#define ENXIO        6
+#define E2BIG        7
+#define ENOEXEC      8
+#define EBADF        9
+#define ECHILD      10
+#define EAGAIN      11
+#define ENOMEM      12
+#define EACCES      13
+#define EFAULT      14
+#define EBUSY       16
+#define EEXIST      17
+#define ENODEV      19
+#define ENOTDIR     20
+#define EISDIR      21
+#define EINVAL      22
+#define ENFILE      23
+#define EMFILE      24
+#define ENOSPC      28
+#define EPIPE       32
+#define ERANGE      34
+#define ENOSYS      38
+#define ENOTEMPTY   39
+#define ELOOP       40
+#define EOVERFLOW   75
+
+#endif
